a868dbbbcfab0192fe6a12c707cd7d584f68ea95
Tsinat Zeree <cheru87@gmail.com>
2016-05-18 00:27:52 -0700

    working gulp structure

---
+++ client/js/controllers/mainCtrl.js
@@ -0,0 +1,12 @@
+
+var app = angular.module('gulpApp');
+
+app.controller('mainCtrl', function($scope) {
+  console.log('HEY!!! mainCtrl!');
+
+  $scope.alert = () => {
+    alert('ALERT!');
+    console.log('this:', this);
+  };
+
+});
